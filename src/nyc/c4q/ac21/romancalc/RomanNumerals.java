8c528a21b489b6bb5b6bdd9284968abad32b1fe9
Hoshiko Oki <hoshikooki@gmail.com>
2015-03-11 00:52:23 -0400

    add test code
    Hopefully it will work

--- src/nyc/c4q/ac21/romancalc/RomanNumerals.java
+++ src/nyc/c4q/ac21/romancalc/RomanNumerals.java
@@ -36,5 +36,28 @@ public class RomanNumerals {
     public static void main(String[] argv) {
         // TODO: Group 3: Write this function!
         // It should test that format() and parse() work correctly.
+        System.out.print(format(18)); /* it should return XVIII*/
+        System.out.print(format(29)); /* it should return XXIX*/
+        System.out.print(format(34)); /* it should return XXXIV*/
+        System.out.print(format(46)); /* it should return XLVI*/
+        System.out.print(format(89)); /* it should return LXXXiX*/
+        System.out.print(format(846)); /* it should return DCCCXLVI*/
+        System.out.print(format(1999)); /* it should return MCMXCIX*/
+        System.out.print(format(2420)); /* it should return MMCDXX*/
+        System.out.print(format(3454)); /* it should return MMMCDLIV*/
+        System.out.print(format(3999)); /* it should return MMMCMXCIX*/
+
+
+
+        System.out.print(parse("XVIII")); /* it should return 18*/
+        System.out.print(parse("XXIX")); /* it should return 29*/
+        System.out.print(parse("XXXIV")); /* it should return 34*/
+        System.out.print(parse("XLVI")); /* it should return 46*/
+        System.out.print(parse("LXXXiX")); /* it should return 89*/
+        System.out.print(parse("DCCCXLVI")); /* it should return 846*/
+        System.out.print(parse("MCMXCIX")); /* it should return 1999*/
+        System.out.print(parse("MMCDXX")); /* it should return 2420*/
+        System.out.print(parse("MMMCDLIV")); /* it should return 3454*/
+        System.out.print(parse("MMMCMXCIX")); /* it should return 3999*/
     }
 }
